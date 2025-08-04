96c222b4db23789fca62fb86ea2c569e0eece8d9
marensovich <bushidik228@mail.ru>
2025-08-04 22:27:14 +0000

    Initial commit

---
+++ eljur/src/main/java/com/marensovich/eljur/EljurApplication.java
@@ -0,0 +1,13 @@
+package com.marensovich.eljur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EljurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EljurApplication.class, args);
+	}
+
+}
