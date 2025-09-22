0817328f0f656672696e90d0007c43653e6fa927
marensovich <bushidik228@mail.ru>
2025-09-22 10:54:55 +0000

    Вынесена логика получения расписания в сервис

--- eljur/src/main/java/com/marensovich/eljur/controller/Web/ScheduleContoller.java
+++ eljur/src/main/java/com/marensovich/eljur/controller/Web/ScheduleContoller.java
@@ -6,6 +6,7 @@ import com.marensovich.eljur.model.*;
 import com.marensovich.eljur.repository.*;
 import com.marensovich.eljur.service.FileService;
 import com.marensovich.eljur.service.ScoreService;
+import com.marensovich.eljur.service.SheduleService;
 import com.marensovich.eljur.service.VisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,25 +27,13 @@ import java.util.stream.Collectors;
 public class ScheduleContoller {
 
     @Autowired
-    private ScheduleRepository lessonRepository;
-    @Autowired
-    private StudentsRepository studentsRepository;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private HomeworkRepository homeworkRepository;
-    @Autowired
-    private SubjectRepository subjectRepository;
-    @Autowired
-    private ScoreRepository scoreRepository;
-    @Autowired
     private JwtUtil jwtUtil;
-    @Autowired
-    private ScoreService scoreService;
     @Autowired
     private VisitService visitService;
     @Autowired
-    private FilesRepository filesRepository;
+    private SheduleService sheduleService;
+    @Autowired
+        private UserRepository userRepository;
 
 
     @CrossOrigin(origins = "http://199.83.103.127:25323", allowCredentials = "true")
@@ -60,59 +49,7 @@ public class ScheduleContoller {
 
         visitService.recordVisit();
 
-        Students student = studentsRepository.getById(user.getId());
-
-        LocalDate now = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-        LocalDate startOfWeek = startDate != null ? LocalDate.parse(startDate, formatter) : now.with(DayOfWeek.MONDAY);
-        LocalDate endOfWeek = endDate != null ? LocalDate.parse(endDate, formatter) : now.with(DayOfWeek.SUNDAY);
-
-        List<Shedule> lessons = lessonRepository.getLessonsByDateBetweenAndGroupAndSubgroup(startOfWeek, endOfWeek, student.getGroup(), student.getSubgroup());
-        TreeMap<String, TreeMap<Integer, Map<String, Object>>> groupedLessons = lessons.stream()
-                .collect(Collectors.groupingBy(
-                        lesson -> lesson.getDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
-                        TreeMap::new,
-                        Collectors.toMap(
-                                Shedule::getRank,
-                                lesson -> {
-                                    Map<String, Object> lessonDetails = new HashMap<>();
-                                    lessonDetails.put("subject", subjectRepository.getSubjectNameById(lesson.getSubject()));
-                                    lessonDetails.put("room", lesson.getRoom());
-                                    lessonDetails.put("teacher", userRepository.getFullnameByUser_id(lesson.getTeacher()));
-                                    homeworkRepository.findHomeworkByLessonID(lesson.getId())
-                                            .map(Homework::getHomework)
-                                            .ifPresent(homework -> lessonDetails.put("homework", homework));
-                                    lessonDetails.put("time", lesson.getTimeStamp());
-                                    FileService fileService = new FileService(filesRepository);
-                                    homeworkRepository.getHomeworkFilesByLessonID(lesson.getId())
-                                            .stream()
-                                            .map(fileService::getFileNameWithID)
-                                            .filter(homeworkFiles -> !homeworkFiles.isEmpty())
-                                            .findFirst()
-                                            .ifPresent(homeworkFiles -> lessonDetails.put("homework_files", homeworkFiles));
-
-                                    List<String> score = scoreRepository.getScoreTypeByLessonIDAndUserId(lesson.getId(), user.getId());
-                                    List<Integer> scores = score.stream()
-                                            .map(scoreService::convertScoreTypeToInt)
-                                            .toList();
-                                    lessonDetails.put("scores", scores);
-
-                                    List<String> scoreWork = scoreRepository.getScoreWorkTypeByLessonIDAndUserId(lesson.getId(), user.getId());
-                                    List<String> scoreWorkTexts = scoreWork.stream()
-                                            .map(scoreService::convertScoreWorkTypeToString)
-                                            .toList();
-                                    lessonDetails.put("scoreWorkType", scoreWorkTexts);
-
-                                    List<String> scoreText = scoreRepository.getScoreTextByLessonIDAndUserId(lesson.getId(), user.getId());
-                                    lessonDetails.put("score_texts", scoreText);
-
-                                    return lessonDetails;
-                                },
-                                (existing, replacement) -> existing,
-                                () -> new TreeMap<>()
-                        )
-                ));
+        TreeMap<String, TreeMap<Integer, Map<String, Object>>> groupedLessons = sheduleService.getLessons(user, startDate, endDate);
 
         return ResponseEntity.status(HttpStatus.OK).body(groupedLessons);
     }
