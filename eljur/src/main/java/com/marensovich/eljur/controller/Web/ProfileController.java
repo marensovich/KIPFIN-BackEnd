39bd2ca8326fe1ac4cddc853bda6e9984bc8cad5
marensovich <bushidik228@mail.ru>
2025-09-22 08:55:59 +0000

    Обновлена логика контроллера для получения данных пользователя.

--- eljur/src/main/java/com/marensovich/eljur/controller/Web/ProfileController.java
+++ eljur/src/main/java/com/marensovich/eljur/controller/Web/ProfileController.java
@@ -26,16 +26,7 @@ public class ProfileController {
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private AdminRepostory adminRepostory;
-    @Autowired
-    private StudentsRepository studentsRepository;
-    @Autowired
-    private TeacherRepository teacherRepository;
-    @Autowired
-    private GroupsRepository groupsRepository;
-    @Autowired
     private JwtUtil jwtUtil;
-    private Serializable ScoreService;
     @Autowired
     private ScoreService scoreService;
     @Autowired
@@ -94,57 +85,10 @@ public class ProfileController {
 
         if (user.isEmpty()) throw new UserNotFoundException("Пользователь не найден");
 
-        return ResponseEntity.status(HttpStatus.OK).body(Map.ofEntries(
-                Map.entry("fullname", user.get().getFullname() != null ? user.get().getFullname() : "Не указано"),
-                Map.entry("post", getPostDescription(user.get().getPost(), userID)),
-                Map.entry("mail", user.get().getEmail() != null ? user.get().getEmail() : "Не указано"),
-                Map.entry("phone", user.get().getPhone() != null ? user.get().getPhone() : "Не указано"),
-                Map.entry("ProfileImage", user.get().getProfileImage() != null ? user.get().getProfileImage() : "Не указано"),
-                Map.entry("TelegramID", user.get().getTelegramID() != null ? user.get().getTelegramID() : "Не указано"),
-                Map.entry("group", getGroupDescription(userID)),
-                Map.entry("avg_score", scoreService.getAvgScoreByUserID(userID)),
-                Map.entry("notificationType", user.get().getNotificationType()),
-                Map.entry("notificationMessages", user.get().isNotificationMessages()),
-                Map.entry("notificationHomework", user.get().isNotificationHomework()),
-                Map.entry("notificationScore", user.get().isNotificationScore()),
-                Map.entry("notificationNews", user.get().isNotificationNews())
-        ));
+        Map profileInfo = profileService.getProfileInfo(user);
 
-    }
+        return ResponseEntity.status(HttpStatus.OK).body(profileInfo);
 
-    private String getGroupDescription(Integer userID) {
-        Optional<Students> studentOptional = studentsRepository.findById(userID);
-        if (studentOptional.isPresent()) {
-            Integer groupId = studentOptional.get().getGroup();
-            Optional<Groups> group = groupsRepository.findById(groupId);
-            if (group.isPresent()) {
-                return group.get().getGroup();
-            } else {
-                return "Группа не найдена для студента";
-            }
-        }
-        Optional<Teacher> teacherOptional = teacherRepository.findTeacherById(userID);
-        if (teacherOptional.isPresent()) {
-            Integer groupId = teacherRepository.getGroupId(userID);
-            if (groupId != null) {
-                Optional<Groups> group = groupsRepository.findById(groupId);
-                return group.map(Object::toString).orElse("Группа не найдена для преподавателя");
-            } else {
-                return "Группа не назначена преподавателю";
-            }
-        }
-
-        return "Пользователь не найден в базе данных студентов или преподавателей";
-    }
-    private String getPostDescription(String post, Integer userID) {
-        return switch (post) {
-            case "student" -> "Студент";
-            case "teacher" -> "Преподаватель";
-            case "admin" -> adminRepostory.findById(userID)
-                    .map(admin -> adminRepostory.getAdminPost(userID))
-                    .orElse("Неизвестный администратор");
-            default -> "Неизвестная должность";
-        };
     }
 }
 
--- eljur/src/main/java/com/marensovich/eljur/service/ProfileService.java
+++ eljur/src/main/java/com/marensovich/eljur/service/ProfileService.java
@@ -2,21 +2,35 @@ package com.marensovich.eljur.service;
 
 import com.marensovich.eljur.data.NotificationType;
 import com.marensovich.eljur.exceptions.Exceptions.InvalidNotificationTypeException;
+import com.marensovich.eljur.model.Groups;
+import com.marensovich.eljur.model.Students;
+import com.marensovich.eljur.model.Teacher;
 import com.marensovich.eljur.model.User;
-import com.marensovich.eljur.repository.UserRepository;
+import com.marensovich.eljur.repository.*;
 import org.springframework.stereotype.Service;
 
 
 import javax.annotation.Nullable;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class ProfileService {
 
     private final UserRepository userRepository;
+    private final ScoreService scoreService;
+    private final StudentsRepository studentsRepository;
+    private final GroupsRepository groupsRepository;
+    private final TeacherRepository teacherRepository;
+    private final AdminRepostory adminRepostory;
 
-    public ProfileService(UserRepository userRepository) {
+    public ProfileService(UserRepository userRepository, ScoreService scoreService, StudentsRepository studentsRepository, GroupsRepository groupsRepository, TeacherRepository teacherRepository, AdminRepostory adminRepostory) {
         this.userRepository = userRepository;
+        this.scoreService = scoreService;
+        this.studentsRepository = studentsRepository;
+        this.groupsRepository = groupsRepository;
+        this.teacherRepository = teacherRepository;
+        this.adminRepostory = adminRepostory;
     }
 
     public void setNotificationSettings(
@@ -49,5 +63,60 @@ public class ProfileService {
             user.get().setNotificationNews(notificationNews);
         }
         userRepository.save(user.get());
+    }
+
+    public Map getProfileInfo(Optional<User> user) {
+        return Map.ofEntries(
+                Map.entry("fullname", user.get().getFullname() != null ? user.get().getFullname() : "Не указано"),
+                Map.entry("post", getPostDescription(user.get().getPost(), user.get().getId())),
+                Map.entry("mail", user.get().getEmail() != null ? user.get().getEmail() : "Не указано"),
+                Map.entry("phone", user.get().getPhone() != null ? user.get().getPhone() : "Не указано"),
+                Map.entry("ProfileImage", user.get().getProfileImage() != null ? user.get().getProfileImage() : "Не указано"),
+                Map.entry("TelegramID", user.get().getTelegramID() != null ? user.get().getTelegramID() : "Не указано"),
+                Map.entry("group", getGroupDescription(user.get().getId())),
+                Map.entry("avg_score", scoreService.getAvgScoreByUserID(user.get().getId())),
+                Map.entry("notificationType", user.get().getNotificationType()),
+                Map.entry("notificationMessages", user.get().isNotificationMessages()),
+                Map.entry("notificationHomework", user.get().isNotificationHomework()),
+                Map.entry("notificationScore", user.get().isNotificationScore()),
+                Map.entry("notificationNews", user.get().isNotificationNews())
+        );
+    }
+
+
+
+    private String getGroupDescription(Integer userID) {
+        Optional<Students> studentOptional = studentsRepository.findById(userID);
+        if (studentOptional.isPresent()) {
+            Integer groupId = studentOptional.get().getGroup();
+            Optional<Groups> group = groupsRepository.findById(groupId);
+            if (group.isPresent()) {
+                return group.get().getGroup();
+            } else {
+                return "Группа не найдена для студента";
+            }
+        }
+        Optional<Teacher> teacherOptional = teacherRepository.findTeacherById(userID);
+        if (teacherOptional.isPresent()) {
+            Integer groupId = teacherRepository.getGroupId(userID);
+            if (groupId != null) {
+                Optional<Groups> group = groupsRepository.findById(groupId);
+                return group.map(Object::toString).orElse("Группа не найдена для преподавателя");
+            } else {
+                return "Группа не назначена преподавателю";
+            }
+        }
+
+        return "Пользователь не найден в базе данных студентов или преподавателей";
+    }
+    private String getPostDescription(String post, Integer userID) {
+        return switch (post) {
+            case "student" -> "Студент";
+            case "teacher" -> "Преподаватель";
+            case "admin" -> adminRepostory.findById(userID)
+                    .map(admin -> adminRepostory.getAdminPost(userID))
+                    .orElse("Неизвестный администратор");
+            default -> "Неизвестная должность";
+        };
     }
 }
