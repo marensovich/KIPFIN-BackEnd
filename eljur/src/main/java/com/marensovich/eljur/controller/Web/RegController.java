3601705402112eeb13e1afb49dd68afd063879fc
marensovich <bushidik228@mail.ru>
2025-09-22 09:01:07 +0000

    Обновлена логика контроллера регистрации пользователей

--- eljur/src/main/java/com/marensovich/eljur/controller/Web/RegController.java
+++ eljur/src/main/java/com/marensovich/eljur/controller/Web/RegController.java
@@ -1,24 +1,12 @@
 package com.marensovich.eljur.controller.Web;
 
-
-
-import com.marensovich.eljur.data.NotificationType;
-import com.marensovich.eljur.exceptions.Exceptions.ActivatedRegistrationCodeException;
-import com.marensovich.eljur.exceptions.Exceptions.InvalidRegistrationCodeException;
-import com.marensovich.eljur.model.*;
-import com.marensovich.eljur.repository.RegKeysRepository;
-import com.marensovich.eljur.repository.StudentsRepository;
-import com.marensovich.eljur.repository.TeacherRepository;
-import com.marensovich.eljur.repository.UserRepository;
-import com.marensovich.eljur.service.RegKeysService;
+import com.marensovich.eljur.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.Map;
 
 @RestController
@@ -26,72 +14,17 @@ import java.util.Map;
 public class RegController {
 
     @Autowired
-    private RegKeysService regKeysService;
-
-    @Autowired
-    private RegKeysRepository regKeysRepository;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private TeacherRepository teacherRepository;
-    @Autowired
-    private StudentsRepository studentsRepository;
+    private AuthService authService;
 
 
     @CrossOrigin(origins = "http://199.83.103.127:25323", allowCredentials = "true")
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestParam String key, @RequestParam String login, @RequestParam String password, HttpServletRequest request) {
-        String code = regKeysService.findRegistrationKey(key);
-        String email = regKeysService.getEmailByRegistrationKey(key);
-        RegKeys regKeys = regKeysRepository.findByRegistrationKey(key)
-                .orElseThrow(() -> new RuntimeException("Код регистрации не найден."));
-        if (code == null) {
-            throw new InvalidRegistrationCodeException("Неправильно введен код регистрации");
-        } else if (regKeysService.getStatusByRegistrationKey(key).equals("Activated")) {
-            throw new ActivatedRegistrationCodeException("Данный код уже активирован.");
+        try {
+            authService.registrationUser(key, login, password, request);
+            return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "Вы успешно прошли регистрацию."));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", "Error while registration user."));
         }
-        Timestamp timestamp = new Timestamp(new Date().getTime());
-        regKeys.setEmail(email);
-        regKeys.setStatus("Activated");
-        regKeys.setActivatedAt(timestamp);
-        String post = regKeys.getPost();
-        User user = new User();
-        user.setEmail(email);
-        user.setPhone(regKeys.getPhone());
-        user.setPassword(password);
-        user.setUsername(login);
-        user.setGroup(regKeys.getGroup());
-        user.setFullname(regKeys.getFullName());
-        user.setRegIP(request.getRemoteAddr());
-        user.setPost(post);
-        user.setLastJoinIP(request.getRemoteAddr());
-        user.setNotificationType(NotificationType.Without_Notification);
-        user.setNotificationHomework(false);
-        user.setNotificationMessages(false);
-        user.setNotificationNews(false);
-        user.setNotificationScore(false);
-        user.setBlack_theme(false);
-        userRepository.save(user);
-        switch (post) {
-            case "student" -> {
-                Students student = new Students();
-                student.setFullname(regKeys.getFullName());
-                student.setGroup(regKeys.getGroup());
-                student.setSubgroup(regKeys.getSubgroup());
-                student.setId(user.getId());
-                studentsRepository.save(student);
-            }
-            case "teacher" -> {
-                Teacher teacher = new Teacher();
-                teacher.setId(user.getId());
-                teacher.setTeacher_groupID(regKeys.getGroup());
-                teacherRepository.save(teacher);
-            }
-            case "admin" -> {
-                Admins admin = new Admins();
-                admin.setId(user.getId());
-            }
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "Вы успешно прошли регистрацию."));
     }
 }
--- eljur/src/main/java/com/marensovich/eljur/service/AuthService.java
+++ eljur/src/main/java/com/marensovich/eljur/service/AuthService.java
@@ -1,16 +1,91 @@
 package com.marensovich.eljur.service;
 
+import com.marensovich.eljur.data.NotificationType;
+import com.marensovich.eljur.exceptions.Exceptions.ActivatedRegistrationCodeException;
+import com.marensovich.eljur.exceptions.Exceptions.InvalidRegistrationCodeException;
+import com.marensovich.eljur.model.*;
+import com.marensovich.eljur.repository.RegKeysRepository;
+import com.marensovich.eljur.repository.StudentsRepository;
+import com.marensovich.eljur.repository.TeacherRepository;
 import com.marensovich.eljur.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 @Service
 public class AuthService {
 
+    private final RegKeysService regKeysService;
+    private final RegKeysRepository regKeysRepository;
+    private final StudentsRepository studentsRepository;
+    private final TeacherRepository teacherRepository;
     private UserRepository userRepository;
+
+    public AuthService(RegKeysService regKeysService, RegKeysRepository regKeysRepository, StudentsRepository studentsRepository, TeacherRepository teacherRepository) {
+        this.regKeysService = regKeysService;
+        this.regKeysRepository = regKeysRepository;
+        this.studentsRepository = studentsRepository;
+        this.teacherRepository = teacherRepository;
+    }
 
     //private PasswordEncoder passwordEncoder;
 
 
-
+    public void registrationUser(String key, String login, String password, HttpServletRequest request) {
+        String code = regKeysService.findRegistrationKey(key);
+        String email = regKeysService.getEmailByRegistrationKey(key);
+        RegKeys regKeys = regKeysRepository.findByRegistrationKey(key)
+                .orElseThrow(() -> new RuntimeException("Код регистрации не найден."));
+        if (code == null) {
+            throw new InvalidRegistrationCodeException("Неправильно введен код регистрации");
+        } else if (regKeysService.getStatusByRegistrationKey(key).equals("Activated")) {
+            throw new ActivatedRegistrationCodeException("Данный код уже активирован.");
+        }
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        regKeys.setEmail(email);
+        regKeys.setStatus("Activated");
+        regKeys.setActivatedAt(timestamp);
+        String post = regKeys.getPost();
+        User user = new User();
+        user.setEmail(email);
+        user.setPhone(regKeys.getPhone());
+        user.setPassword(password);
+        user.setUsername(login);
+        user.setGroup(regKeys.getGroup());
+        user.setFullname(regKeys.getFullName());
+        user.setRegIP(request.getRemoteAddr());
+        user.setPost(post);
+        user.setLastJoinIP(request.getRemoteAddr());
+        user.setNotificationType(NotificationType.Without_Notification);
+        user.setNotificationHomework(false);
+        user.setNotificationMessages(false);
+        user.setNotificationNews(false);
+        user.setNotificationScore(false);
+        user.setBlack_theme(false);
+        userRepository.save(user);
+        switch (post) {
+            case "student" -> {
+                Students student = new Students();
+                student.setFullname(regKeys.getFullName());
+                student.setGroup(regKeys.getGroup());
+                student.setSubgroup(regKeys.getSubgroup());
+                student.setId(user.getId());
+                studentsRepository.save(student);
+            }
+            case "teacher" -> {
+                Teacher teacher = new Teacher();
+                teacher.setId(user.getId());
+                teacher.setTeacher_groupID(regKeys.getGroup());
+                teacherRepository.save(teacher);
+            }
+            case "admin" -> {
+                Admins admin = new Admins();
+                admin.setId(user.getId());
+            }
+        }
+    }
 
 }
