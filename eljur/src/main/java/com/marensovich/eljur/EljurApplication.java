f8397eb6aa6fe2fea66cbe1f747a8f0ed29af575
marensovich <bushidik228@mail.ru>
2025-09-23 22:46:48 +0000

    added DotEnv loader, fixed error "No static resource favicon.ico."

--- eljur/src/main/java/com/marensovich/eljur/EljurApplication.java
+++ eljur/src/main/java/com/marensovich/eljur/EljurApplication.java
@@ -1,5 +1,6 @@
 package com.marensovich.eljur;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,12 +10,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EljurApplication {
 
+
+
 	/**
 	 * The entry point of application.
 	 *
 	 * @param args the input arguments
 	 */
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./") // path to .env file
+				.ignoreIfMissing()
+				.load();
+
+		// load system env variables
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
 		SpringApplication.run(EljurApplication.class, args);
 	}
 
