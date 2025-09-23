c63351924cb3642ef0fbeeb35e676dfb92369080
marensovich <bushidik228@mail.ru>
2025-09-23 06:52:02 +0000

    убран прямой токен

--- eljur/src/main/java/com/marensovich/eljur/config/TelegramNotifier/TelegramBotConfig.java
+++ eljur/src/main/java/com/marensovich/eljur/config/TelegramNotifier/TelegramBotConfig.java
@@ -7,8 +7,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TelegramBotConfig {
 
+    String botToken = System.getenv("TELEGRAM_LOGGER_TOKEN");
+    String chatId = System.getenv("TELEGRAM_LOGGER_CHAT_ID");
+
     @Bean
     public TelegramBotNotifier telegramBotNotifier() {
-        return new TelegramBotNotifier("7639553720:AAFFd33rhCZEYjKngnZaB1f2MNfnJ8L82Gc", "6737078498");
+        return new TelegramBotNotifier(botToken, chatId);
     }
 }
