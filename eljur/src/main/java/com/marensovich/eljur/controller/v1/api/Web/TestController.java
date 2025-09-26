d17f6d637f57a6ce3c9b44cd1cd7dd62f805c516
marensovich <bushidik228@mail.ru>
2025-09-26 06:13:09 +0000

    added test controller to test any DTO and another components of the project

---
+++ eljur/src/main/java/com/marensovich/eljur/controller/v1/api/Web/TestController.java
@@ -0,0 +1,25 @@
+package com.marensovich.eljur.controller.v1.api.Web;
+
+
+import com.marensovich.eljur.model.Groups;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * REST controller for testing DTO and another components of the project
+ *
+ * @author marensovich
+ * @version v.0.1
+ * @since v.0.1
+ */
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+
+}
